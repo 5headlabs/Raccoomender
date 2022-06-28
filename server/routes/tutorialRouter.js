afdfde12538a981206eebafe3fc1b1b37925ffc4
MalteJosten <malte.josten@stud.uni-due.de>
2022-06-28 10:27:50 +0200

    [MOD] Modified tutorialController to use auth middleware.

--- server/routes/tutorialRouter.js
+++ server/routes/tutorialRouter.js
@@ -2,11 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const tutorialController = require('../controllers/tutorialController');
+const authController = require("../controllers/authController");
 
-router.post("/create", tutorialController.createTutorial);
+router.post("/create", authController.checkLoginStatus, tutorialController.createTutorial);
 router.get("/list", tutorialController.listTutorial);
 
 router.get("/view/:id", tutorialController.getTutorial);
+
+router.post("/add/comment", authController.checkLoginStatus, commentController.addComment, tutorialController.addComment);
 
 
 module.exports = router;
