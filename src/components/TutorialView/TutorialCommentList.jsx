3ed2ac27f3009cbe7465a8f94fa29251e4ca852f
Alexander Hochhalter <alexander.hochhalter@stud.uni-due.de>
2022-07-04 22:29:45 +0200

    implemented TutorialCommentList

--- src/components/TutorialView/TutorialCommentList.jsx
+++ src/components/TutorialView/TutorialCommentList.jsx
@@ -1,12 +1,26 @@
-import { Card } from "@mui/material";
 import React from "react";
+import { Grid, Typography, Card, Divider } from "@mui/material";
 
 export default function TutorialCommentList(props) {
   const { values } = props;
 
   return (
     <>
-      <Card>c</Card>
+      <Card>
+        <Grid container sx={{ p: 2 }}>
+          {values.comments.map((comment) => (
+            <Grid item key={comment._id} xs={12}>
+              <Typography align="left" fontWeight="bold" variant="h6" gutterBottom>
+                {comment.title}
+              </Typography>
+              <Typography align="left">{comment.content}</Typography>
+              <Typography align="right" fontSize="9pt">Comment by:</Typography>
+              <Typography align="right" fontSize="9pt">{comment.author}</Typography>
+              <Divider />
+            </Grid>
+          ))}
+        </Grid>
+      </Card>
     </>
   );
 }
