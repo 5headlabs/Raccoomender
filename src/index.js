5254fc272b4288a5701b4b6cf8cdfb275506e0e1
Alexander Hochhalter <alexander.hochhalter@stud.uni-due.de>
2022-08-07 15:20:46 +0200

    change proxy settings

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-export const API_URL = 'http://localhost:3001/api';
+export const API_URL = '/api';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
