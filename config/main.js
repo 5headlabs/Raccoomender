5417e0b44667697aa53ab4c1e84750cd21184f54
Alexander Hochhalter <alexander.hochhalter@stud.uni-due.de>
2022-08-07 14:45:02 +0200

    changed to process port

--- config/main.js
+++ config/main.js
@@ -3,8 +3,8 @@ module.exports = {
 
   database: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.sextv.mongodb.net/${process.env.MONGO_DBNAME}?retryWrites=true&w=majority`,
   
-  port:      3001,
-  test_port: 3001,
+  port:      process.env.PORT || 3001,
+  test_port: process.env.PORT || 3001,
   test_db:   'test',
   test_env:  'test'
 };
--- index.js
+++ index.js
@@ -26,13 +26,6 @@ console.log(`Your server is running on port ${config.port}.`);
 
 // CORS
 app.use(cors());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-});
 
 // Define routes and their handlers
 app.use("/api/auth",     authRouter);
