184132b67315f506f41e6c94e99b2ee2c4f26d75
Julien Lukasewycz <julien.lukasewycz@uni-due.de>
2022-08-07 12:15:21 +0200

    [ADDED] text when no comments exist to TutorialCommentList

--- src/components/TutorialViewComponents/TutorialCommentList.jsx
+++ src/components/TutorialViewComponents/TutorialCommentList.jsx
@@ -8,26 +8,31 @@ export default function TutorialCommentList(props) {
     <>
       <Card>
         <Grid container sx={{ p: 2 }}>
-          {values.comments.map((comment) => (
-            <Grid item key={comment._id} xs={12}>
-              <Typography
-                align="left"
-                fontWeight="bold"
-                variant="h6"
-                gutterBottom
-              >
-                {comment.title}
-              </Typography>
-              <Typography align="left">{comment.content}</Typography>
-              <Typography align="right" fontSize="9pt">
-                Comment by:
-              </Typography>
-              <Typography align="right" fontSize="9pt">
-                {comment.author.username}
-              </Typography>
-              <Divider />
-            </Grid>
-          ))}
+          {values.comments.length === 0 ? (
+            <Typography variant="h7" color="#4B6584" margin="auto">
+              No comments yet.
+            </Typography>
+          ) : (
+            values.comments.map((comment) => (
+              <Grid item key={comment._id} xs={12}>
+                <Typography
+                  align="left"
+                  fontWeight="bold"
+                  variant="h6"
+                  gutterBottom
+                >
+                  {comment.title}
+                </Typography>
+                <Typography align="left">{comment.content}</Typography>
+                <Typography align="right" fontSize="9pt">
+                  Comment by:
+                </Typography>
+                <Typography align="right" fontSize="9pt">
+                  {comment.author.username}
+                </Typography>
+                <Divider />
+              </Grid>
+          )))}
         </Grid>
       </Card>
     </>
